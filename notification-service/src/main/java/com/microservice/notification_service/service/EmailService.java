9264deac19dbf90126d1af12f0f19f5848a6b85b
NguyenChauTinh <chautinh05122@gmail.com>
2025-03-07 16:39:31 +0700

    test send email

--- notification-service/src/main/java/com/microservice/notification_service/service/EmailService.java
+++ notification-service/src/main/java/com/microservice/notification_service/service/EmailService.java
@@ -2,11 +2,17 @@ package com.microservice.notification_service.service;
 
 import com.microservice.notification_service.dto.reponse.EmailResponse;
 import com.microservice.notification_service.dto.request.EmailRequest;
+import com.microservice.notification_service.dto.request.SendEmailRequest;
+import com.microservice.notification_service.dto.request.Sender;
 import com.microservice.notification_service.repository.httpclient.EmailClient;
+import feign.FeignException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.context.ApplicationContextException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +21,22 @@ public class EmailService {
 
     EmailClient emailClient;
 
-    public EmailResponse sendEmail(String apiKey, EmailRequest request) {
-        return emailClient.sendEmail(apiKey, request);
+    String apiKey = "your-brevo-apikey";
+
+    public EmailResponse sendEmail(SendEmailRequest request) {
+        EmailRequest emailRequest = EmailRequest.builder()
+                .sender(Sender.builder()
+                        .name("Flex Shoes")
+                        .email("chautinh05122@gmail.com")
+                        .build())
+                .to(List.of(request.getTo()))
+                .subject(request.getSubject())
+                .htmlContent(request.getHtmlContent())
+                .build();
+        try {
+            return emailClient.sendEmail(apiKey, emailRequest);
+        } catch (FeignException e){
+            throw new ApplicationContextException("Failed to send email", e);
+        }
     }
 }
