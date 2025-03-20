8553824ebd62187e55bc2ce71378ad244bbbeb7d
hominhhau <hauxoan333@gmail.com>
2025-03-20 15:31:21 +0700

    add docker compose

--- payment-service/src/main/java/com/microservice/payment_service/mapper/PaymentVNPayDetailMapper.java
+++ payment-service/src/main/java/com/microservice/payment_service/mapper/PaymentVNPayDetailMapper.java
@@ -2,11 +2,44 @@ package com.microservice.payment_service.mapper;
 
 import com.microservice.payment_service.dto.PaymentVNPayDetailDto;
 import com.microservice.payment_service.entity.PaymentVNPayDetail;
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface PaymentVNPayDetailMapper {
 
-    PaymentVNPayDetailDto toPaymentVNPayDetailDto(PaymentVNPayDetail payDetail);
-    PaymentVNPayDetail toPaymentVNPayDetail(PaymentVNPayDetailDto payDetailDto);
+@Component
+public class PaymentVNPayDetailMapper {
+
+    public PaymentVNPayDetailDto toPaymentVNPayDetailDto(PaymentVNPayDetail payDetail) {
+        if ( payDetail == null ) {
+            return null;
+        }
+
+        PaymentVNPayDetailDto paymentVNPayDetailDto = new PaymentVNPayDetailDto();
+
+        paymentVNPayDetailDto.setPaymentId( payDetail.getPaymentId() );
+        paymentVNPayDetailDto.setTransactionId( payDetail.getTransactionId() );
+        paymentVNPayDetailDto.setBankCode( payDetail.getBankCode() );
+        paymentVNPayDetailDto.setPaymentTime( payDetail.getPaymentTime() );
+        paymentVNPayDetailDto.setVnPayResponse( payDetail.getVnPayResponse() );
+        paymentVNPayDetailDto.setPayment( payDetail.getPayment() );
+
+        return paymentVNPayDetailDto;
+    }
+
+
+    public PaymentVNPayDetail toPaymentVNPayDetail(PaymentVNPayDetailDto payDetailDto) {
+        if ( payDetailDto == null ) {
+            return null;
+        }
+
+        PaymentVNPayDetail paymentVNPayDetail = new PaymentVNPayDetail();
+
+        paymentVNPayDetail.setPaymentId( payDetailDto.getPaymentId() );
+        paymentVNPayDetail.setTransactionId( payDetailDto.getTransactionId() );
+        paymentVNPayDetail.setBankCode( payDetailDto.getBankCode() );
+        paymentVNPayDetail.setPaymentTime( payDetailDto.getPaymentTime() );
+        paymentVNPayDetail.setVnPayResponse( payDetailDto.getVnPayResponse() );
+        paymentVNPayDetail.setPayment( payDetailDto.getPayment() );
+
+        return paymentVNPayDetail;
+    }
 }
