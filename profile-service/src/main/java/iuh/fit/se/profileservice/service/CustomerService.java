3ac201a8756bb43891673298f738b5ed6793c4f3
baddevp <20114581.tien@student.iuh.edu.vn>
2025-03-26 12:58:11 +0700

    profile-service fix catching error and openfeign

--- profile-service/src/main/java/iuh/fit/se/profileservice/service/CustomerService.java
+++ profile-service/src/main/java/iuh/fit/se/profileservice/service/CustomerService.java
@@ -1,13 +1,17 @@
 package iuh.fit.se.profileservice.service;
 
+import iuh.fit.se.profileservice.dtos.ApiResponse;
 import iuh.fit.se.profileservice.dtos.CustomerDTO;
+import iuh.fit.se.profileservice.exceptions.ProfileAlreadyExistsException;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface CustomerService {
 
-        List<CustomerDTO> getAllCustomer();
-        CustomerDTO save(CustomerDTO customersDTO);
-        CustomerDTO findByID(Integer id);
-        CustomerDTO updateByID(Integer id, CustomerDTO customerDTO);
+    ResponseEntity<ApiResponse<?>>  getAllCustomer();
+    ResponseEntity<ApiResponse<?>> createCustomer(CustomerDTO customersDTO) throws ProfileAlreadyExistsException;
+    ResponseEntity<ApiResponse<?>> findByID(Long id);
+    ResponseEntity<ApiResponse<?>> updateByID(Long id, CustomerDTO customerDTO);
+
     }
