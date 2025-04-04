00a6f96dec790e4df2a716c12150c374676e162e
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-04-04 23:18:45 +0700

    validation

--- order-service/src/main/java/com/microservice/order_service/controller/InvoiceController.java
+++ order-service/src/main/java/com/microservice/order_service/controller/InvoiceController.java
@@ -3,6 +3,7 @@ package com.microservice.order_service.controller;
 import com.microservice.order_service.dto.InvoiceDto;
 import com.microservice.order_service.dto.MyAPIResponse;
 import com.microservice.order_service.service.InvoiceService;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -28,7 +29,7 @@ import java.util.List;
 
 		// Tạo hóa đơn mới
 		@PostMapping("/add")
-		public ResponseEntity<InvoiceDto> createInvoice(@RequestBody InvoiceDto invoiceDto) {
+		public ResponseEntity<InvoiceDto> createInvoice(@Valid @RequestBody InvoiceDto invoiceDto) {
 			return ResponseEntity.ok(invoiceService.saveInvoice(invoiceDto));
 		}
 
@@ -41,7 +42,7 @@ import java.util.List;
 
 		// Cập nhật hóa đơn
 		@PutMapping("/updateInvoice")
-		public MyAPIResponse<Boolean> updateInvoice(@RequestBody InvoiceDto invoiceDto) {
+		public MyAPIResponse<Boolean> updateInvoice( @Valid @RequestBody InvoiceDto invoiceDto) {
 			return MyAPIResponse.<Boolean>builder().result(invoiceService.updateInvoice(invoiceDto)).build();
 		}
 
--- order-service/src/main/java/com/microservice/order_service/dto/InvoiceDetailDto.java
+++ order-service/src/main/java/com/microservice/order_service/dto/InvoiceDetailDto.java
@@ -1,5 +1,7 @@
 package com.microservice.order_service.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -10,9 +12,15 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class InvoiceDetailDto {
 	Integer detailId;
+
+	@NotNull(message = "Invoice ID là bắt buộc")
 	Integer invoiceId;
+
+	@NotNull(message = "Product ID là bắt buộc")
 	Integer productId;
+
+	@Min(value = 1, message = "Quantity phải lớn hơn 0")
 	int quantity;
 
-	ProductDto product; // ✅ Thêm đối tượng ProductDto vào đây
+	ProductDto product;
 }
