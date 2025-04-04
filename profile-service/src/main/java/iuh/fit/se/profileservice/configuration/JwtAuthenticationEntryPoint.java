9c1ad459513cb3a7622c27dcc446d65ee54dda97
NguyenChauTinh <chautinh05122@gmail.com>
2025-04-05 00:19:01 +0700

    Config profile service jwt

---
+++ profile-service/src/main/java/iuh/fit/se/profileservice/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package iuh.fit.se.profileservice.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import iuh.fit.se.profileservice.dtos.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .status("FAILED")
+                .message("Unauthorized")
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
