640da1071dc32f4e3a8d72c5f778e0c180994f2d
NguyenChauTinh <chautinh05122@gmail.com>
2025-04-05 01:51:22 +0700

    Config CORS policy for api-gateway

--- profile-service/src/main/java/iuh/fit/se/profileservice/configuration/SecurityConfig.java
+++ profile-service/src/main/java/iuh/fit/se/profileservice/configuration/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private static final String[] PUBLIC_ENDPOINTS = {
-
+            "/",
     };
 
     private final CustomJwtDecoder customJwtDecoder;
