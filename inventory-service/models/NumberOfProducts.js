bde58e582b1aa83564ef7a25b52bdd6360d7df09
21034451_NguyenThiQuynhGiang <nguyenquynhgiang1324@gmail.com>
2025-04-06 12:23:10 +0700

    Change schema

--- inventory-service/models/NumberOfProducts.js
+++ inventory-service/models/NumberOfProducts.js
@@ -6,21 +6,9 @@ const NumberOfProductsSchema = new mongoose.Schema({
         required: true
     },
     size: {
-        // _id: {
-        //     type: mongoose.Schema.Types.ObjectId, ref: 'Size'
-        // },
-        // sizeName: {
-        //     type: String,
-        // }
         type: mongoose.Schema.Types.ObjectId, ref: 'Size'
     },
     color:{
-        // _id: {
-        //     type: mongoose.Schema.Types.ObjectId, ref: 'Color'
-        // },
-        // colorName: {
-        //     type: String,
-        // }
         type: mongoose.Schema.Types.ObjectId, ref: 'Color'
     },
 });
--- inventory-service/routes/productRoutes.js
+++ inventory-service/routes/productRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/getAllProducts', productController.getAllProducts);
 router.get('/getAllProducts/:id', productController.getProductById);
+router.post('/filterProducts', productController.getFilteredProducts);
 
 
 module.exports = router;
