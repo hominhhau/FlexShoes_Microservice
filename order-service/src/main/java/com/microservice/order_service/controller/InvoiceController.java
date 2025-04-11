f8d4c477def06d8509a7d66f3569c2cff3e1f39b
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-04-11 22:59:12 +0700

    fix port

--- order-service/src/main/java/com/microservice/order_service/controller/InvoiceController.java
+++ order-service/src/main/java/com/microservice/order_service/controller/InvoiceController.java
@@ -13,13 +13,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-//	@CrossOrigin(origins = "http://localhost:3000")
 	@RestController
 	@RequestMapping("/invoices")
 	@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 	@RequiredArgsConstructor
-	@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.HEAD, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.POST, RequestMethod.DELETE}, allowedHeaders = {"Content-Type", "Authorization"})
-// Hoặc trên từng method:
+//	@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.HEAD, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.POST, RequestMethod.DELETE}, allowedHeaders = {"Content-Type", "Authorization"})
 	public class InvoiceController {
 		InvoiceService invoiceService;
 
