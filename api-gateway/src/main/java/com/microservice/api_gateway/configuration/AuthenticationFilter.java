1b70bd87595d186aeca2a5e5b9ddf435660e1cf6
hominhhau <hauxoan333@gmail.com>
2025-04-25 18:57:01 +0700

    done payment vs vnpay

--- api-gateway/src/main/java/com/microservice/api_gateway/configuration/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/microservice/api_gateway/configuration/AuthenticationFilter.java
@@ -54,8 +54,9 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
             "/inventory/getAllProductTypes",
             "/inventory/getAllBrandTypes",
             "/inventory/chatgpt",
-
-
+            "/inventory/update",
+            "/payment/create_payment",
+            "/payment/payment-return",
 
     };
 
