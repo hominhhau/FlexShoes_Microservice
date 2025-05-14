1b9a91fda7a70e5af331f74ec2a88b9fd464cd6d
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-05-15 03:08:21 +0700

    fix order

--- inventory-service/routes/productRoutes.js
+++ inventory-service/routes/productRoutes.js
@@ -7,7 +7,7 @@ router.get("/getAllProducts", productController.getAllProducts);
 router.get("/getAllProducts/:id", productController.getProductById);
 router.post("/createProduct", uploadMultiple, productController.createProduct);
 router.post("/update", uploadMultiple, productController.update);
-router.post("/purchase", productController.purchase);
+router.put("/purchase", productController.purchase);
 router.post("/delete/:id", productController.deleteProductById);
 router.get("/searchProduct", productController.searchProduct);
 // router.post('/filterProducts', productController.getFilteredProducts);
